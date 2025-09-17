b3d637cfb9a7453f206733c931f9ef74537b4a0f
Daniel <crunchyhotline@gmail.com>
2025-09-16 20:18:22 -0700

    upd

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { route } from 'quasar/wrappers'
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory  } from 'vue-router'
 import routes from './routes'
 // import router from './routes'
 
@@ -13,7 +13,7 @@ import routes from './routes'
  */
 
 export default route(function (/* { store, ssrContext } */) {
-  const createHistory = createWebHashHistory
+  const createHistory = createWebHistory
   /*process.env.SERVER
     ? createMemoryHistory
     : (process.env.VUE_ROUTER_MODE === 'history' ? createWebHistory : createWebHashHistory)*/
